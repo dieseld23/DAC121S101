964abd8af237828ae5e144a8a2790a10f2afc71e
Dan M <dieseld23@users.noreply.github.com>
2021-11-05 10:43:51 -0400

    Add files via upload

---
+++ DAC121S101.cpp
@@ -0,0 +1,96 @@
+/**
+*  Arduino Library for Texas Instruments DAC121S101 - 12-Bit Micro Power, RRO Digital-to-Analog Converter
+*  
+*  @author Dan Maslach <dmaslach@gmail.com>
+*/
+
+/**
+ * The MIT License
+ *
+ * Copyright 2021 Dan Maslach <dmaslach@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "DAC121S101.h"
+#include <Arduino.h>
+
+
+/*
+ * Constructor of the class
+ * @param io_pin_cs a byte indicating the pin to be use as the chip select pin (CS)
+ */
+DAC121::DAC121(uint8_t io_pin_cs) {
+    cs = io_pin_cs;
+}
+
+/*
+ * This method initializes the SPI port and sets default state to normal mode and data to an initial setpoint;
+ */
+void DAC121::begin(uint8_t mode, uint16_t initData) {
+    SPI.begin();
+    modeSet = mode;
+    setData(initData);
+
+}
+
+/*
+ * This method just made to set the mode globally for the chip
+ */
+void DAC121::setPowerDownMode(uint8_t mode) {
+     modeSet = mode;
+
+}
+
+/*
+ * This sets the DAC to your preferred voltage using a given reference voltage (read from an IC's analog input) so it can set the step as close to your given set voltage
+ */
+
+void DAC121::setVoltage(float Vref, float setVolts) {
+    uint16_t digiVolts = round((setVolts * 4096) / Vref);
+    setData(digiVolts);
+}
+
+/*
+ * This sends the data via SPI. Takes the mode previously set, and data and sends it.
+ */
+
+void DAC121::setData(uint16_t data) {    
+    uint16_t maskedMode = (modeSet << 12) & PDM_MASK;
+	uint16_t maskedData = data & DATA_MASK;
+
+    uint16_t sendData = maskedMode | maskedData;     
+
+    uint8_t byteLow = sendData & 0xff;
+    uint8_t byteHigh = (sendData >> 8);
+
+    SPI.beginTransaction(SPISettings(SCLK, MSBFIRST, SPI_MODE1));
+    
+    // Send config
+    digitalWrite(cs, LOW);
+    delayMicroseconds(10);
+    SPI.transfer(byteHigh);
+    SPI.transfer(byteLow);
+   
+    digitalWrite(cs, HIGH);
+    delayMicroseconds(10);
+    
+    SPI.endTransaction();
+
+}
---
+++ DAC121S101.h
@@ -0,0 +1,70 @@
+/**
+*  Arduino Library for Texas Instruments DAC121S101 - 12-Bit Micro Power, RRO Digital-to-Analog Converter
+*  
+*  @author Dan Maslach <dmaslach@gmail.com>
+*/
+
+/**
+ * The MIT License
+ *
+ * Copyright 2021 Dan Maslach <dmaslach@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef DAC121_h
+#define DAC121_h
+
+#include <Arduino.h>
+#include <SPI.h>
+
+
+#define DATA_MASK       0b1111111111111
+#define PDM_MASK		0b11 << 12
+#define PDM_BIT_1_POS	13
+#define PDM_BIT_0_POS	12
+
+#define PDM_NORMAL		0b00
+#define PDM_2_5K_GND	0b01
+#define PDM_100K_GND	0b10
+#define PDM_HIGH_IMP	0b11
+
+/*
+ * Class representing the DAC121 sensor chip
+ */
+class DAC121 {
+    public:
+        DAC121(uint8_t io_pin_cs);                                 ///< Constructor
+
+        void begin(uint8_t mode, uint16_t initData);
+      
+        void setData(uint16_t data);
+        void setVoltage(float Vref, float setVolts);
+        void setPowerDownMode(uint8_t mode);      
+        // Bit constant
+        const long int SCLK        = 10000000;       ///< DAC121 SCLK frequency: 30000000 Hz Maximum for DAC121 (30Mhz)
+
+       
+    private:
+        uint8_t cs;                                 ///< Chip select pin (choose one)
+        uint8_t modeSet;
+    
+};
+
+#endif
